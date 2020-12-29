7415ad5d801cdbe774a4338f17e1f81cc2762ea1
LeeTrent <lee.trent.1@gmail.com>
2020-12-29 06:35:10 -0600

    ThreadPool now implements Drop trait

--- TheBook/Projects/hello/src/lib.rs
+++ TheBook/Projects/hello/src/lib.rs
@@ -36,12 +36,25 @@ impl ThreadPool {
   }
 }
 
+impl Drop for ThreadPool {
+  fn drop(&mut self) {
+    for worker in &mut self.workers {
+      println!("Shutting down worker {}", worker.id);
+
+      if let Some(thread) = worker.thread.take() {
+        thread.join().unwrap();
+      }
+    }
+  }
+}
+
 struct Worker {
   id: usize,
   //thread: thread::JoinHandle<()>,
   //thread: thread::JoinHandle<std::sync::mpsc::Receiver<Job>>,
   //thread: Arc<Mutex<mpsc::Receiver<Job>>>,
-  thread: thread::JoinHandle<Arc<Mutex<mpsc::Receiver<Job>>>>,
+  //thread: thread::JoinHandle<Arc<Mutex<mpsc::Receiver<Job>>>>,
+  thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -52,6 +65,9 @@ impl Worker {
       job();
     });
 
-    Worker { id, thread }
+    Worker {
+      id,
+      thread: Some(thread),
+    }
   }
 }
